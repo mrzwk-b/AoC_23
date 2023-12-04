59a8a18f0cad6624f462ebe61b90987650d7696d
mrzwk-b <bmerkey@gmail.com>
2023-12-04 02:23:24 -0500

    completed pt 1 made progress on pt 2

--- day_3_gear_ratios/src/main.rs
+++ day_3_gear_ratios/src/main.rs
@@ -1,55 +1,146 @@
 use std::fs;
 
-fn is_symbol (character: u8) -> bool {
-    !(character == b'.' || (b'0'..b'9').contains(&character))
+struct FormattedSource<'a> {
+    content: &'a Vec<u8>,
+    row_length: usize,
+    bottom_row: usize
+}
+
+fn is_symbol(character: u8) -> bool {
+    !(character == b'.' || (48..58).contains(&character))
+}
+
+fn get_part_length(
+    start: usize, 
+    FormattedSource{
+        content,
+        row_length,
+        bottom_row
+    }: &FormattedSource
+) -> Option<usize> 
+{
+    let mut length = 0;
+    let mut hit_symbol = false;
+    let mut i = start;
+    while i < content.len() && (48..58).contains(&content[i]) {
+        let column = i % row_length;
+        if i == start {
+            hit_symbol |= 
+                (column != 0 && (
+                    (i > *row_length && is_symbol(content[i - *row_length - 1])) ||
+                    (i < *bottom_row && is_symbol(content[i + *row_length - 1])) ||
+                    is_symbol(content[i - 1])
+                )) ||
+                (i > *row_length && is_symbol(content[i - *row_length])) ||
+                (i < *bottom_row && is_symbol(content[i + *row_length]))
+            ;
+        }
+        if column != row_length - 2 {
+            hit_symbol |=
+                (i > *row_length && is_symbol(content[i - *row_length + 1])) ||
+                (i < *bottom_row && is_symbol(content[i + *row_length + 1])) ||
+                is_symbol(content[i + 1])
+            ;
+        }
+        i += 1;
+        length += 1;
+
+    }
+    if !hit_symbol || length == 0 {
+        return None;
+    }
+    return Some(length);
+}
+
+fn get_part_value(
+    start: usize,
+    length: usize,
+    FormattedSource{content, ..}: &FormattedSource
+) -> usize
+{
+    let mut digits_total = 0;
+    for i in 0..length {
+        digits_total = (digits_total * 10) + (content[start + i] as usize - 48);
+    }
+    return digits_total;
+}
+
+fn pt_1(source: FormattedSource) -> usize {  
+    let mut parts_total = 0;
+    let mut i = 0;
+    while i < source.content.len() {
+        if let Some(length) = get_part_length(i, &source) {
+            parts_total += get_part_value(i, length, &source);
+            i += length;
+        } 
+        else {
+            i += 1;
+        }
+    }
+    parts_total
+}
+
+// still incomplete
+fn pt_2(FormattedSource{content, row_length, bottom_row}: FormattedSource) -> usize {
+    let mut gears_total = 0;
+    let mut i = 0;
+    while i < content.len() {
+        if content[i] == b'*' {
+            let row_start =  i - (i % row_length);
+            let mut parts: Vec<(usize, usize)> = Vec::new();
+            let mut above_countdown = 0;
+            let mut level_countdown = 0;
+            let mut below_countdown = 0;
+            let mut j = row_start;
+            while j < row_length {
+                //above
+                if i > row_length && above_countdown == 0 {
+                    if let Some(length) = get_part_length(row_start - row_length + j, &FormattedSource {content, row_length, bottom_row}) {
+                        parts.push((row_start - row_length + j, length));
+                        above_countdown = length - 1;
+                    }
+                }
+                //level
+                if level_countdown == 0 {
+                    if let Some(length) =
+                        get_part_length(row_start + j, &FormattedSource {content, row_length, bottom_row}) 
+                    {
+                        parts.push((row_start + j, length));
+                        level_countdown = length - 1;
+                    }
+                }
+                //below
+                if i < bottom_row && below_countdown == 0 {
+                    if let Some(length) =
+                        get_part_length(row_start + row_length + j, &FormattedSource {content, row_length, bottom_row}) 
+                    {
+                        parts.push((row_start + row_length + j, length));
+                        below_countdown = length - 1;
+                    }
+                }
+                j += 1;
+            }
+            if parts.len() == 2 {
+                gears_total += 
+                    get_part_value(parts[0].0, parts[0].1, &FormattedSource{content, row_length, bottom_row}) *
+                    get_part_value(parts[1].0, parts[1].1, &FormattedSource{content, row_length, bottom_row})
+                ;
+            }
+        }
+        i += 1;
+    }
+    gears_total
 }
 
 fn main() {
-    // read input
-    let source = fs::read("input.txt").expect("failed to read input file");
+    let content = &fs::read("calibration.txt").expect("failed to read input file");
     let mut row_length = 0;
-    while source[row_length] != b'\n' {
+    while content[row_length] != b'\n' {
         row_length += 1;
     }
     row_length += 1;
-    let bottom_row = source.len() - row_length;
-    // iterate over it
-    let mut in_digits = false;
-    let mut hit_symbol = false;
-    let mut digits_total = 0;
-    let mut schema_total = 0;
-    for i in 0..source.len() {
-        if (48..58).contains(&source[i]) {
-            // check for symbols only in places where the grid exists and hasn't been checked in this digit string yet
-            let column = i % row_length;
-            if !in_digits {
-                hit_symbol |= 
-                    (column != 0 && (
-                        (i > row_length && is_symbol(source[i - row_length - 1])) ||
-                        (i < bottom_row && is_symbol(source[i + row_length - 1])) ||
-                        is_symbol(source[i - 1])
-                    )) ||
-                    (i > row_length && is_symbol(source[i - row_length])) ||
-                    (i < bottom_row && is_symbol(source[i + row_length]))
-                ;
-            }
-            if column != row_length - 2 {
-                hit_symbol |=
-                    (i > row_length && is_symbol(source[i - row_length + 1])) ||
-                    (i < bottom_row && is_symbol(source[i + row_length + 1])) ||
-                    is_symbol(source[i + 1])
-                ;
-            }
-            digits_total = (digits_total * 10) + (source[i] as usize - 48);
-            in_digits = true;
-        }
-        else if in_digits {
-            schema_total += digits_total * (hit_symbol as usize);
-            digits_total = 0;
-            hit_symbol = false;
-            in_digits = false;
-        }
-        
-    }
-    println!("{}", schema_total);
+    let bottom_row = content.len() - row_length;
+    let source = FormattedSource{content, row_length, bottom_row};
+
+    println!("{}", pt_2(source));
 }
