54f8b0e9b391e2d32fd3e6990f5cf69f06ea7877
mrzwk-b <bmerkey@gmail.com>
2023-12-05 22:56:01 -0500

    day 5 *

--- day_5_seed_fertilizer/src/main.rs
+++ day_5_seed_fertilizer/src/main.rs
@@ -1,12 +1,12 @@
-use std::{fs, env};
+use std::{fs, env, collections::HashMap, usize, ops::Range};
 
-// returned tuple formatted as (destination start, source start, range lenght) just like in document
-fn parse_line(input: &Vec<u8>, index: &mut usize) -> [usize; 3] {
+// returned tuple formatted as (destination start, source start, range size) just like in document
+fn parse_line(almanac: &Vec<u8>, index: &mut usize) -> [usize; 3] {
     let mut line_values: [usize; 3] = [0, 0, 0];
     for i in 0..3 {
         let mut total = 0;
-        while (48..58).contains(&input[*index]) {
-            total = (total * 10) + (input[*index] as usize - 48);
+        while (48..58).contains(&almanac[*index]) {
+            total = (total * 10) + (almanac[*index] as usize - 48);
             *index += 1;
         }
         line_values[i] = total;
@@ -15,28 +15,56 @@ fn parse_line(input: &Vec<u8>, index: &mut usize) -> [usize; 3] {
     return line_values;
 }
 
-fn remap(input: &Vec<u8>, start_data: &Vec<usize>, index: &mut usize) -> Vec<usize> {
-    let mut end_data = start_data.clone();
-    while input[*index] != b'\n' {
-        let map = parse_line(&input, index);
-        for j in 0..start_data.len() {
-            if (map[1]..map[2]).contains(&start_data[j]) {
-                end_data[j] = map[0] + (start_data[j] - map[1]);
+fn next_map(almanac: &Vec<u8>, index: &mut usize) -> Option<HashMap<Range<usize>, Range<usize>>> {
+    while *index < almanac.len() && !(48..58).contains(&almanac[*index]) {*index += 1;}
+    let mut map = HashMap::new();
+    while *index < almanac.len() && (48..58).contains(&almanac[*index]) {
+        let [destination_start, source_start, range_size] = parse_line(&almanac, index);
+        map.insert(source_start..source_start + range_size, destination_start..destination_start + range_size);
+    }
+    if map.len() == 0 {
+        return None;
+    }
+    return Some(map);
+}
+
+// doesn't actually need to take index but it's more convenient for the rest of the program that it advances
+fn get_seeds(almanac: &Vec<u8>, index: &mut usize) -> Vec<usize> {
+    while !(48..58).contains(&almanac[*index]) {*index += 1;}
+    let mut seeds = Vec::new();
+    while almanac[*index] != b'\n' {
+        let mut total = 0;
+        while (48..58).contains(&almanac[*index]) {
+            total = (total * 10) + (almanac[*index] as usize - 48);
+            *index += 1;
+        }
+        seeds.push(total);
+        *index += 1;
+    }
+    return seeds;
+}
+
+fn pt_1(almanac: Vec<u8>) -> usize {
+    let mut index = 0;
+    let mut prev_data = get_seeds(&almanac, &mut index);
+    let mut next_data = prev_data.clone();
+    while let Some(map) = next_map(&almanac, &mut index) {
+        for i in 0..prev_data.len() {
+            for r in map.keys() {
+                if r.contains(&prev_data[i]) {
+                    next_data[i] = map.get(r).unwrap().start + (prev_data[i] - r.start);
+                }
             }
         }
+        prev_data = next_data.clone();
     }
-    return end_data;
+    return *prev_data.iter().min().unwrap();
 }
 
-fn pt_1(input: Vec<u8>) -> usize {
-    let mut table: [Vec<usize>; 8];
-
-    0
-}
-
+// code designed for LF .txt input
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let input = fs::read(&args[1]).expect("failed to read input");
+    let almanac = fs::read(&args[1]).expect("failed to read input");
     
-    println!("{}", pt_1(input));
+    println!("{}", pt_1(almanac));
 }
--- day_5_seed_fertilizer/target/debug/day_5_seed_fertilizer.d
+++ day_5_seed_fertilizer/target/debug/day_5_seed_fertilizer.d
@@ -1,1 +1,1 @@
-C:\users\ben\documents\aoc_23\day_5_seed_fertilizer\target\debug\day_5_seed_fertilizer.exe: C:\users\ben\documents\aoc_23\day_5_seed_fertilizer\src\main.rs
+C:\Users\bmerk\small\ projects\AoC_23\day_5_seed_fertilizer\target\debug\day_5_seed_fertilizer.exe: C:\Users\bmerk\small\ projects\AoC_23\day_5_seed_fertilizer\src\main.rs
